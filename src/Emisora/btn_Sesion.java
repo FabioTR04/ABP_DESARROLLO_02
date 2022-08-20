1643452282567d7dcbe3ca1e73c7f298c0e04bbd
Fabio <fabiotru04@gmail.com}>
2022-08-20 11:23:34 -0500

    new 2 desde ramaf

--- src/Emisora/btn_Sesion.java
+++ src/Emisora/btn_Sesion.java
@@ -1,17 +1,13 @@
 package Emisora;
 
 import interfaz.Iniciar_Sesion;
-package Emisora;
 
 /**
  *
  * @author fabiotrujillo
  */
 public class btn_Sesion {
-    Iniciar_Sesion is = new Iniciar_Sesion();
-    if(valido=true){
+   
     
-    }else{
     
-    }
 }
